6c372f63b7f575b95f89a96b4e45966d774d7752
Lewis Moronta <lewis.moronta@gmail.com>
2018-11-04 20:17:39 -0500

    adds strings, comments, and ws to lexer

--- src/scanner.rs
+++ src/scanner.rs
@@ -26,7 +26,7 @@ impl Scanner {
             self.start = self.current;
             self.scan_token(rox); // uses Rox::error
         }
-        self.add_token(TokenType::Eof);
+        self.add_token(TokenType::Eof, "\0".to_string());
     }
     fn is_at_end(&self) -> bool {
         self.current >= self.get_source_chars().len()
@@ -36,46 +36,78 @@ impl Scanner {
     }
     fn scan_token(&mut self, rox: &mut Rox) {
         match self.advance() {
-            '(' => self.add_token(TokenType::LeftParen),
-            ')' => self.add_token(TokenType::RightParen),
-            '{' => self.add_token(TokenType::LeftBrace),
-            '}' => self.add_token(TokenType::RightBrace),
-            ',' => self.add_token(TokenType::Comma),
-            '.' => self.add_token(TokenType::Dot),
-            '-' => self.add_token(TokenType::Minus),
-            '+' => self.add_token(TokenType::Plus),
-            ';' => self.add_token(TokenType::Semicolon),
-            '*' => self.add_token(TokenType::Star),
+            '"' => self.string(rox),
+            '(' => self.add_token(TokenType::LeftParen, "(".to_string()),
+            ')' => self.add_token(TokenType::RightParen, ")".to_string()),
+            '{' => self.add_token(TokenType::LeftBrace, "{".to_string()),
+            '}' => self.add_token(TokenType::RightBrace, "}".to_string()),
+            ',' => self.add_token(TokenType::Comma, ",".to_string()),
+            '.' => self.add_token(TokenType::Dot, ".".to_string()),
+            '-' => self.add_token(TokenType::Minus, "-".to_string()),
+            '+' => self.add_token(TokenType::Plus, "+".to_string()),
+            ';' => self.add_token(TokenType::Semicolon, ";".to_string()),
+            '*' => self.add_token(TokenType::Star, "*".to_string()),
             '!' => if self._match('=') {
-                self.add_token(TokenType::BangEqual)
+                self.add_token(TokenType::BangEqual, "!=".to_string())
             } else {
-                self.add_token(TokenType::Bang)
+                self.add_token(TokenType::Bang, "!".to_string())
             },
             '=' => if self._match('=') {
-                self.add_token(TokenType::EqualEqual)
+                self.add_token(TokenType::EqualEqual, "==".to_string())
             } else {
-                self.add_token(TokenType::Equal)
+                self.add_token(TokenType::Equal, "=".to_string())
             },
             '<' => if self._match('=') {
-                self.add_token(TokenType::LessEqual)
+                self.add_token(TokenType::LessEqual, "<=".to_string())
             } else {
-                self.add_token(TokenType::Less)
+                self.add_token(TokenType::Less, "<".to_string())
             },
             '>' => if self._match('=') {
-                self.add_token(TokenType::GreaterEqual)
+                self.add_token(TokenType::GreaterEqual, ">=".to_string())
             } else {
-                self.add_token(TokenType::Greater)
+                self.add_token(TokenType::Greater, ">".to_string())
             },
+            '/' => {
+                let mut comment = String::new();
+                if self._match('/') {
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        comment.push(self.advance());
+                    }
+                    self.add_token(TokenType::Comment, comment.as_str().trim().to_string());
+                } else {
+                    self.add_token(TokenType::Slash, "/".to_string());
+                }
+            }
+            '\n' => self.line += 1,
+            ' ' | '\r' | '\t' => {}
             c @ _ => Rox::error(self.line, format!("Unrecognized character: {}", c), rox),
         }
     }
-    fn add_token(&mut self, _type: TokenType) {
+    fn add_token(&mut self, _type: TokenType, literal: String) {
         self.tokens.push(Token {
             _type: _type,
             lexeme: None,
-            literal: None,
+            literal: Some(literal),
             line: self.line,
         });
+    }
+    fn string(&mut self, rox: &mut Rox) {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+        if self.is_at_end() {
+            Rox::error(self.line, "Unterminated string.".to_string(), rox);
+        } else {
+            self.advance(); // The closing "
+            let content = self.trim_quotes();
+            self.add_token(TokenType::String, content)
+        }
+    }
+    fn trim_quotes(&self) -> String {
+        self.source[self.start + 1..self.current - 1].to_string()
     }
     fn advance(&mut self) -> char {
         self.current = self.current + 1;
@@ -90,6 +122,13 @@ impl Scanner {
         }
         self.current = self.current + 1;
         true
+    }
+    fn peek(&self) -> char {
+        if self.is_at_end() {
+            '\0'
+        } else {
+            self.get_source_char(self.current)
+        }
     }
     fn get_source_char(&self, index: usize) -> char {
         self.get_source_chars()[index]
--- src/token_type.rs
+++ src/token_type.rs
@@ -1,21 +1,51 @@
 #[derive(Debug)]
 pub enum TokenType {
-  // Single-character tokens.                      
-  LeftParen, RightParen, LeftBrace, RightBrace,
-  Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+  // Single-character tokens.
+  LeftParen,
+  RightParen,
+  LeftBrace,
+  RightBrace,
+  Comma,
+  Dot,
+  Minus,
+  Plus,
+  Semicolon,
+  Slash,
+  Star,
 
-  // One or two character tokens.                  
-  Bang, BangEqual,
-  Equal, EqualEqual,
-  Greater, GreaterEqual,
-  Less, LessEqual,
+  // One or two character tokens.
+  Bang,
+  BangEqual,
+  Equal,
+  EqualEqual,
+  Greater,
+  GreaterEqual,
+  Less,
+  LessEqual,
+  Comment,
 
-  // Literals.                                     
-  Identifier, String, Number,
+  // Literals.
+  Identifier,
+  String,
+  Number,
 
-  // Keywords.                                     
-  And, Class, Else, False, Fun, For, If, Nil, Or,
-  Print, Return, Super, This, True, Var, While,
+  // Keywords.
+  And,
+  Class,
+  Else,
+  False,
+  Fun,
+  For,
+  If,
+  Nil,
+  Or,
+  Print,
+  Return,
+  Super,
+  This,
+  True,
+  Var,
+  While,
 
   Eof,
 }
